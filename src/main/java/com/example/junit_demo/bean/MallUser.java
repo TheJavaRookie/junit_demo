92a1c9ebc13ea00adedfdfb32cd915cd67fea732
star <v5v5v5v@yeah.net>
2022-05-05 15:36:29 +0800

    添加用户实体类

---
+++ src/main/java/com/example/junit_demo/bean/MallUser.java
@@ -0,0 +1,15 @@
+package com.example.junit_demo.bean;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author star
+ * @date 2022/5/5 15:29
+ * @since 1.0.0
+ */
+
+@Component
+public class MallUser {
+
+    private String name;
+}
